41bc019179e40d45cbff5b4293453fda51704be8
JallaJenkins <wakeupalready@gmail.com>
2024-06-18 15:43:55 -0700

    Added projectList, sidebar formatting, no selected project page basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import Sidebar from "./components/Sidebar";
+import NoProjects from './components/NoProjects';
+
+const TEST_LIST = [
+  {
+    projectName: "Learning React",
+    selected: true,
+  },
+  {
+    projectName: "Mastering React",
+    selected: false,
+  }
+];
+
+// console.log(TEST_LIST);
+
 
 function App() {
+
+  const [projectList, setProjectList] = useState([]);
+  const [addingProject, setAddingProject] = useState(false);
+
+  // setTaskList(TEST_LIST);
+
+
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </main>
+    <>
+      <main className="h-screen my-8 flex gap-8">
+        <Sidebar projectList={TEST_LIST} />
+        <NoProjects />
+      </main>
+    </>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,31 @@
-export default function Sidebar() {
+export default function Sidebar({ projectList }) {
+
+  const classSelectedItem = "mb-4 bg-stone-800 hover:bg-stone-700";
+  const classItem = "mb-4 text-stone-400 hover:bg-stone-700 hover:text-stone-300";
+  const classItemButton = "w-full text-left";
+
+  console.log(projectList);
+
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl ">
-      {/* <h1 className="my-8 text-left text-2xl font-semibold ">YOUR PROJECTS</h1> */}
+
       <h2 className="mb-8 font-bold uppercase md:text-2xl text-stone-200">YOUR PROJECTS</h2>
+
+      <button className="mb-4 bg-stone-700 text-stone-400 px-3 py-2 rounded-md text-lg hover:bg-stone-600 hover:text-stone-100">+ Add project</button>
+
+      <ul className="mt-8 text-stone-200 text-xl">
+
+        {projectList.map(project => {
+          return (<li
+            key={project.projectName}
+            className={project.selected ? classSelectedItem : classItem}>
+            <button className={classItemButton}>
+              {project.projectName}
+            </button>
+          </li>)
+        })}
+      </ul>
+
     </aside>
   );
 }
