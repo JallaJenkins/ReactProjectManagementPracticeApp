3e71e1d503df2feb2bd564999749e3cbe4ece758
JallaJenkins <wakeupalready@gmail.com>
2024-06-17 20:59:33 -0700

    Added basic sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl ">
+      {/* <h1 className="my-8 text-left text-2xl font-semibold ">YOUR PROJECTS</h1> */}
+      <h2 className="mb-8 font-bold uppercase md:text-2xl text-stone-200">YOUR PROJECTS</h2>
+    </aside>
+  );
+}
